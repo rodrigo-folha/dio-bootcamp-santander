6ae6041c925c8874c6dd10f4d49d033cf7a7b425
Rodrigo <rodrigofomo@gmail.com>
2024-05-20 12:23:21 -0300

    Desenvolvendo desafio de projeto com Design Patterns GoF usando Spring Framework

---
+++ spring/README.md
@@ -0,0 +1,6 @@
+## Módulo - Ganhando Produtividade no Java com Spring Framework
+
+## Desafio de projeto
+
+### Design Patterns com Java: Dos Clássicos (GoF) ao Spring Framework
+Neste desafio foi explorado o conceito de Padrões de Projeto na prática, implementando soluções que utilizem os padrões clássicos do GoF (Gang of Four) e as abstrações fornecidas pelo Spring Framework
---
+++ spring/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Cliente;
+
+/**
+ * Interface que definie o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface
+ *
+ * @author rodrigo-folha
+ */
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir(Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+
+}
